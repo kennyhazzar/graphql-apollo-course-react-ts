3b1e219359614cc04dffb82ed3e27b34e76da7c5
kennyhazzar <56494597+kennyhazzar@users.noreply.github.com>
2022-05-21 18:49:30 +0700

    create graphql routes and init react ts app

---
+++ server/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+require('dotenv').config()
+const { graphqlHTTP } = require('express-graphql')
+const cors = require('cors')
+const schema = require('./schema')
+
+const PORT = process.env.PORT
+const users = [{ id: 1, username: "Vasya", age: 25 }]
+
+const app = express()
+app.use(cors())
+
+const createUser = (input) => {
+    const id = Date.now()
+    return {
+        id,
+        ...input
+    }
+}
+
+const root = {
+    getAllUsers: () => {
+        return users
+    },
+    getUser: ({ id }) => {
+        return users.find(user => user.id == id)
+    },
+    createUser: ({ input }) => {
+        const user = createUser(input)
+        users.push(user)
+        return user
+    }
+}
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root
+}))
+
+app.listen(PORT, () => console.log(`server has been started on ${PORT}`))
